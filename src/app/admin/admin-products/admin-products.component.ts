68007415b7f6bea73694e59836cb69a928151d64
unknown <joawa10@yahoo.com>
2020-05-10 20:47:39 +0800

    create data table and provide data

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { ProductService } from 'src/app/product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Product } from 'src/app/models/product';
+import { DataTableResource } from 'angular7-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -12,10 +13,31 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[]; //fix
   filteredProducts: any[]; //fix
   subscription: Subscription; //fix
+  tableResource: DataTableResource<Product>;
+  items: Product [];
+  itemCount: number;
 
   constructor(private productService: ProductService) { //fix
     this.subscription = this.productService.getAll() //has snapshotChanges in fix product key commit
-    .subscribe(products => this.filteredProducts = this.products = products); 
+    .subscribe(products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products);
+    }); 
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset:0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);
   }
 
   filter(query: string) { //fix
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { AdminAuthGuardService as AdminAuthGuard } from './admin-auth-guard.serv
 import { UserService } from './user.service';
 import { AuthGuardService as AuthGuard, AuthGuardService } from './auth-guard.service';
 import { AuthService } from './auth.service';
-import { LoginComponent } from './login/login.component';
-import { RouterModule } from '@angular/router';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CustomFormsModule } from 'ng2-validation';
-import { DataTableModule } from 'angular-4-data-table';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular7-data-table';
+
+import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
@@ -28,8 +26,10 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -49,7 +49,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     BrowserModule,
     FormsModule,
     CustomFormsModule,
-    DataTableModule,
+    DataTableModule.forRoot(),
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
