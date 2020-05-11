9894dbf2535fe85563da679722dc6c7ddef3e5ad
unknown <joawa10@yahoo.com>
2020-05-11 14:19:25 +0800

    add display quantity when adding to cart

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from './../shopping-cart.service';
 import { ProductService } from './../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/switchMap';
@@ -9,15 +11,18 @@ import 'rxjs/add/operator/switchMap';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy{
   products: Product[] = [];
   filteredProducts: Product[];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     route: ActivatedRoute,
     productService: ProductService,
-  ) { 
+    private shoppingCartService: ShoppingCartService
+  ) {     
 
     productService
       .getAll()
@@ -32,5 +37,14 @@ export class ProductsComponent {
           this.products.filter(p => p.category === this.category) :
           this.products;
       });
+  }
+
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart()).valueChanges() //add valucChanges()
+      .subscribe(cart => this.cart = cart); 
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
