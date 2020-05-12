806153626dc3d37e799a419d12ae4413e7eeb082
unknown <joawa10@yahoo.com>
2020-05-12 13:02:18 +0800

    add clear cart button and functionality

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -14,17 +14,30 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  private create() {
-    return this.db.list('/shopping-carts').push({
-      dateCreated: new Date().getTime()
-    });
-  }
-
   async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId).snapshotChanges() //add snapshotChanges
       .pipe(map(x => new ShoppingCart(x.payload.exportVal()?.items))); //add elvis operator
   }
+
+  async addToCart(product: Product) {
+    this.updateItem(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItem(product, -1);
+  }
+
+  async clearCart() {
+    let cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
+  private create() {
+    return this.db.list('/shopping-carts').push({
+      dateCreated: new Date().getTime()
+    });
+  }  
 
   private getItem(cartId: string, productId: string) {
     return this.db.object('/shopping-carts/' + cartId + '/items/' + productId); //can add <any>
@@ -37,33 +50,8 @@ export class ShoppingCartService {
     let result = await this.create();
     localStorage.setItem('cartId', result.key);
     return result.key;   
-  }
-
-  async addToCart(product: Product) {
-    this.updateItem(product, 1);
-  }
-
-  async removeFromCart(product: Product) {
-    this.updateItem(product, -1);
-  }
-
-  // private async updateItemQuantity(product: Product, change: number) {
-  //   let cartId = await this.getOrCreateCartId();
-  //   let item$ = this.getItem(cartId, product.key);
-  //   item$
-  //     .snapshotChanges()
-  //     .pipe(take(1))
-  //     .subscribe(item => {
-  //       let quantity = (item?.payload.child("/quantity").val() || 0) + change;
-  //       if(quantity == 0) item$.remove();
-  //       else item$.update({
-  //         title: product.title,
-  //         imageUrl:product.imageUrl,
-  //         price: product.price,
-  //         quantity: quantity
-  //       });
-  //     });
-  // }
+  }  
+  
   private async updateItem(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item$: Observable<any> = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key).valueChanges();
@@ -80,3 +68,21 @@ export class ShoppingCartService {
   });
   }
 }
+
+// private async updateItemQuantity(product: Product, change: number) {
+  //   let cartId = await this.getOrCreateCartId();
+  //   let item$ = this.getItem(cartId, product.key);
+  //   item$
+  //     .snapshotChanges()
+  //     .pipe(take(1))
+  //     .subscribe(item => {
+  //       let quantity = (item?.payload.child("/quantity").val() || 0) + change;
+  //       if(quantity == 0) item$.remove();
+  //       else item$.update({
+  //         title: product.title,
+  //         imageUrl:product.imageUrl,
+  //         price: product.price,
+  //         quantity: quantity
+  //       });
+  //     });
+  // }
