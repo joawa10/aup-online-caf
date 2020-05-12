c2c10240abb2ee8e4acf0e75abc38346f5077aaf
unknown <joawa10@yahoo.com>
2020-05-12 11:26:38 +0800

    improve codefor initializingthe items array object

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -7,5 +7,9 @@ export class ShoppingCartItem {
     price: number;
     quantity: number;
 
+    constructor(init?: Partial<ShoppingCartItem>) { //init can be an object that looks like a ShoppingCartItem object
+        Object.assign(this, init); //copy properties of 'init' to 'this'
+    } 
+
     get totalPrice() { return this?.price * this.quantity; } //added elvis operator
 }
