2e5b6bc2bada9aebd263a157a7d0e0894d93ff46
unknown <joawa10@yahoo.com>
2020-05-13 00:23:15 +0800

    create core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { ShoppingModule } from './shopping/shopping.module';
 import { AdminModule } from './admin/admin.module';
 import { SharedModule } from './shared/shared.module';
@@ -19,10 +20,10 @@ import { AdminProductsComponent } from './admin/components/admin-products/admin-
 import { ProductFormComponent } from './admin/components/product-form/product-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { BsNavbarComponent } from './core/components/bs-navbar/bs-navbar.component';
 import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { LoginComponent } from './core/components/login/login.component';
 import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
 import { OrderSuccessComponent } from './shopping/components/order-success/order-success.component';
 import { ProductFilterComponent } from './shopping/components/products/product-filter/product-filter.component';
@@ -36,14 +37,13 @@ import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopp
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent,
-    HomeComponent,    
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
     ShoppingModule,
+    CoreModule,
     FormsModule,
     CustomFormsModule,
     DataTableModule.forRoot(),
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    BsNavbarComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([])
+  ],
+  exports: [
+    BsNavbarComponent
+  ]
+})
+export class CoreModule { }
