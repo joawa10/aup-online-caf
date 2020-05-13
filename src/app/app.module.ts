57a72c77c758b01d116f6a03df0e38a8ae685089
unknown <joawa10@yahoo.com>
2020-05-13 13:49:51 +0800

    add homepage and add carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HomeComponent } from './core/components/home/home.component';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
@@ -11,8 +13,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './core/components/login/login.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { ProductsComponent } from './shopping/components/products/products.component';
 import { ShoppingModule } from './shopping/shopping.module';
+// import { ProductsComponent } from './shopping/components/products/products.component';
 
 
 @NgModule({
@@ -28,7 +30,7 @@ import { ShoppingModule } from './shopping/shopping.module';
     AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: '', component: ProductsComponent },      
+      { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },  
     ]),    
   ],
--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarousel]
+  ,styles: [` .img-fluid{ min-width:100%}`]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 1000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+    // config.showNavigationArrows = true;
+    // config.showNavigationIndicators = true;
   }
 
+ 
 }
