8bf57abfa0a3bd2b0132bb34715496d80b604afd
unknown <joawa10@yahoo.com>
2020-05-18 16:08:45 +0800

    move orderview component to shopping module

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -8,6 +8,7 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
 import { CheckOutComponent } from './components/check-out/check-out.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { OrdersViewComponent } from './components/orders-view/orders-view.component';
 import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
 import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
 import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
@@ -21,6 +22,7 @@ import { AuthGuardService as AuthGuard } from './../shared/services/auth-guard.s
     ShoppingCartComponent,
     CheckOutComponent,
     OrderSuccessComponent,
+    OrdersViewComponent,
     MyOrdersComponent,
     ProductFilterComponent,    
     ShoppingCartSummaryComponent,
@@ -34,6 +36,7 @@ import { AuthGuardService as AuthGuard } from './../shared/services/auth-guard.s
       { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
       { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard]  },
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]  },
+      { path: 'orders/view', component: OrdersViewComponent, canActivate: [AuthGuard]  },
     ])
   ]
 })
